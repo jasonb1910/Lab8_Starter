4192d7e0209e759a1ee1da415048c0cba3e39fc7
Jason Bui <j8bui@ucsd.edu>
2023-05-27 02:00:08 -0700

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab 8 - Starter
+
+### Jason Bui
+
+1. I would run my automated tests for my Recipe project within a GitHub action that runs whenever code is pushed because by testing each time new code is added, I can find and fix any issues right away.Then, if the code of my team is tested, it can make working together easier. It can also help ensure the code is of good quality.
+2. No, to check if a function is returning the correct output, I should use unit tests, not end-to-end tests.
+3. Yes, unit tests can test individual parts of the "message" feature. But to fully test the feature, I also need integration tests and end-to-end tests.
+4. Yes, I would use a unit test to test the "max message length" feature because it checks a specific, isolated function of the application.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,84 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('(123) 456-7890 is a valid phone number', () => {;
+    expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+
+test('(123) 456-7890 is a valid phone number', () => {;
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('1234 is not a valid phone number', () => {;
+    expect(functions.isPhoneNumber('123-456')).toBe(false);
+});
+
+test('abcd is not a valid phone number', () => {;
+    expect(functions.isPhoneNumber('hellothere')).toBe(false);
+});
+
+test('j8bui@ucsd.edu is a valid email', () => {;
+    expect(functions.isEmail('j8bui@ucsd.edu')).toBe(true);
+});
+
+test('dakaene@gmail.com is a valid email', () => {;
+    expect(functions.isEmail('dakaene@gmail.com')).toBe(true);
+});
+
+test('studentucsd.edu is not a valid email', () => {;
+    expect(functions.isEmail('studentucsd.edu')).toBe(false);
+});
+
+test('thisisfake is not a valid email', () => {;
+    expect(functions.isEmail('thisisfake')).toBe(false);
+});
+
+test('Password110 is a valid strong password', () => {;
+    expect(functions.isStrongPassword('Password110')).toBe(true);
+});
+
+test('password_110 is a valid strong password', () => {;
+    expect(functions.isStrongPassword('password_110')).toBe(true);
+});
+
+test('110 is not a valid strong password', () => {;
+    expect(functions.isStrongPassword('110')).toBe(false);
+});
+
+test('universityofcaliforniasandiego is not a valid strong password', () => {;
+    expect(functions.isStrongPassword('universityofcaliforniasandiego')).toBe(false);
+});
+
+test('02/02/2020 is a valid date', () => {;
+    expect(functions.isDate('02/02/2020')).toBe(true);
+});
+
+test('2/2/2222 is a valid date', () => {;
+    expect(functions.isDate('2/2/2222')).toBe(true);
+});
+
+test('2/2/22 is not a valid date', () => {;
+    expect(functions.isDate('2/2/22')).toBe(false);
+});
+
+test('NotDate is not a valid date', () => {;
+    expect(functions.isDate('NotDate')).toBe(false);
+});
+
+test('#008000 is a valid hex color', () => {;
+    expect(functions.isHexColor('#008000')).toBe(true);
+});
+
+test('#FF0000 is a valid hex color', () => {;
+    expect(functions.isHexColor('#FF0000')).toBe(true);
+});
+
+test('white is not a valid hex color', () => {;
+    expect(functions.isHexColor('white')).toBe(false);
+});
+
+test('#A is not a valid hex color', () => {;
+    expect(functions.isHexColor('#A')).toBe(false);
+});
+
